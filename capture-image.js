ee3b756b4bf5b66cb9b15e2f6afbe237e94fc432
Manuel Juan Fosela Águila <manufosela@gmail.com>
2020-01-26 18:44:10 +0100

    fixed mask position and add reset button

--- capture-image.js
+++ capture-image.js
@@ -1,4 +1,4 @@
-import { LitElement, html, css } from 'lit-element';
+import { LitElement, html, css, unsafeCSS } from 'lit-element';
 
 /**
  * `capture-image`
@@ -10,8 +10,10 @@ import { LitElement, html, css } from 'lit-element';
  * @demo demo/index.html
  */
 
- class CaptureImage extends LitElement {
-  static get is() { return 'capture-image'; }
+export default class CaptureImage extends LitElement {
+  static get is() {
+    return 'capture-image';
+  }
 
   static get properties() {
     return {
@@ -22,7 +24,7 @@ import { LitElement, html, css } from 'lit-element';
     };
   }
 
-  static get styles(){
+  static get styles() {
     return css`
       :host {
         display: block;
@@ -35,6 +37,13 @@ import { LitElement, html, css } from 'lit-element';
       #canvas{
         position:absolute;
       } 
+      #mask {
+        position:absolute;
+        top:0;
+        left:0;
+        width:${unsafeCSS(this.sizeX)}px;
+        height:${unsafeCSS(this.sizeY)}px;
+      }
       #mirror {
         position:absolute;
       }
@@ -58,7 +67,7 @@ import { LitElement, html, css } from 'lit-element';
       }
     `;
   }
-  
+
   constructor() {
     super();
     this.sizeX = 640;
@@ -79,13 +88,13 @@ import { LitElement, html, css } from 'lit-element';
     this.video = this.renderRoot.querySelector('#video');
     this.saveBtn = this.renderRoot.querySelector('#save');
 
-    if (this.mask) { this.addMask(); }
+    if (this.mask) {
+      this.addMask();
+    }
 
-    this.canvas.width = 640 - this.masksizeX*2;
-    this.canvas.height = 480 - this.masksizeY*2;
+    this.canvas.width = this.sizeX - this.masksizeX * 2;
+    this.canvas.height = this.sizeY - this.masksizeY * 2;
     this.mirror = this.renderRoot.querySelector('#mirror');
-    //this.mirror.width = this.canvas.width;
-    //this.mirror.height = this.canvas.height;
 
     this.main();
   }
@@ -93,17 +102,18 @@ import { LitElement, html, css } from 'lit-element';
   addMask() {
     this.mask = this.renderRoot.querySelector('#mask');
     this.maskCtx = this.mask.getContext('2d');
-    this.masksizeX = this.sizeX * this.maskpercent/200;
-    this.masksizeY = this.sizeY * this.maskpercent/200;
+    this.masksizeX = this.sizeX * this.maskpercent / 200;
+    this.masksizeY = this.sizeY * this.maskpercent / 200;
     this.maskCtx.fillStyle = 'rgba(0, 0, 0, 0.5)';
     this.maskCtx.fillRect(0, 0, this.sizeX, this.masksizeY);
-    this.maskCtx.fillRect(0, this.sizeY-this.masksizeY, this.sizeX, this.masksizeY);
-    this.maskCtx.fillRect(0, this.masksizeY, this.masksizeX, this.sizeY-this.masksizeY*2);
-    this.maskCtx.fillRect(this.sizeX-this.masksizeX, this.masksizeY, this.masksizeX, this.sizeY-this.masksizeY*2);
+    this.maskCtx.fillRect(0, this.sizeY - this.masksizeY, this.sizeX, this.masksizeY);
+    this.maskCtx.fillRect(0, this.masksizeY, this.masksizeX, this.sizeY - this.masksizeY * 2);
+    this.maskCtx.fillRect(this.sizeX - this.masksizeX, this.masksizeY, this.masksizeX, this.sizeY - this.masksizeY * 2);
+    this.shadowRoot.querySelector('#botonera').style.width = this.sizeX + 'px';
   }
 
   main() {
-    if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
       navigator.mediaDevices.getUserMedia({ video: true }).then((stream) => {
         this.video.srcObject = stream;
         this.video.play();
@@ -113,40 +123,36 @@ import { LitElement, html, css } from 'lit-element';
 
   snapImage() {
     let snapImage = this.video;
-    this.context.drawImage(snapImage, 640 * this.maskpercent/200, 480 * this.maskpercent/200, 640-(640 * this.maskpercent/200) * 2, 480-(480* this.maskpercent/200)*2, 0, 0, 640-(640 * this.maskpercent/200)*2, 480-(480 * this.maskpercent/200)*2);
-    //this.context.drawImage(snapImage, this.masksizeX, this.masksizeY, this.sizeX-this.masksizeX*2, this.sizeY-this.masksizeY*2, 0, 0, this.sizeX-this.masksizeX*2, this.sizeY-this.masksizeY*2);
-    this.saveBtn.style.visibility = "visible";
+    this.context.drawImage(snapImage, this.sizeX * this.maskpercent / 200, this.sizeY * this.maskpercent / 200, this.sizeX - (this.sizeX * this.maskpercent / 200) * 2, this.sizeY - (this.sizeY * this.maskpercent / 200) * 2, 0, 0, this.sizeX - (this.sizeX * this.maskpercent / 200) * 2, this.sizeY - (this.sizeY * this.maskpercent / 200) * 2);
+    this.saveBtn.style.visibility = 'visible';
+    this.canvas.style.left = (this.sizeX / 2 - this.canvas.width / 2) + 'px';
+    this.canvas.style.top = (this.sizeY / 2 - this.canvas.height / 2) + 'px';
+  }
+
+  resetImage() {
+    this.context.clearRect(0, 0, this.sizeX, this.sizeY);
   }
 
   saveImage() {
     let dataURL = this.canvas.toDataURL('image/png');
     let d = new Date();
-    let now = "" + d.getYear() + d.getMonth() + d.getDate() + d.getHours() + d.getMinutes() + d.getSeconds(); 
-    this.saveBtn.download = "snap"+ now;
+    let now = '' + d.getYear() + d.getMonth() + d.getDate() + d.getHours() + d.getMinutes() + d.getSeconds();
+    this.saveBtn.download = 'snapBtn' + now;
     this.saveBtn.href = dataURL;
   }
 
   render() {
     return html`
-      <style>
-        #mask {
-          width:${this.sizeX}px;
-          height:${this.sizeY}px;
-          position:absolute;
-          top:0;
-        }
-      </style>
       <div id="botonera">
-        <button id="snap" @click="${this.snapImage}">Snap Photo</button>
+        <button id="snapBtn" @click="${this.snapImage}">Snap Photo</button>
+        <button id="resetBtn" @click="${this.resetImage}">Reset Capture</button>
         <a href="#" id="save" @click="${this.saveImage}" download>Save Photo</a>
       </div>
       <div id="container">
         <video id="video" width="${this.sizeX}" height="${this.sizeY}" autoplay></video>
         <canvas id="mask" width="${this.sizeX}" height="${this.sizeY}"></canvas>
         <canvas id="canvas" width="${this.sizeX}" height="${this.sizeY}"></canvas>
-        <!--<img src="" id="mirror" class="canvas__mirror" style="display:none"/>-->
       </div>
-      
     `;
   }
 }
